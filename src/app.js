df210c29953604f88b71d72d42071caf4ff0edc4
Marina Gallardo <marinagallardo@gmail.com>
2023-05-04 17:09:57 +0200

    WIP animation responsive

--- src/app.js
+++ src/app.js
@@ -1,37 +1,78 @@
 import { cursorComponent } from "./cursor";
 import { gsap } from "gsap";
+import { setSplitText } from "./setSplitText";
 
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
+const arrow = document.querySelector('.slider__content svg');
+const initialPosition = -260;
+
+const drawLines = () => {
+  const lines = document.querySelectorAll('.line');
+
+  console.log('lines', lines);
+
+  gsap.set(lines, { backgroundColor: '#D6FFAC', yPercent: 100 });
+  gsap.fromTo(lines, { yPercent: 100 }, { yPercent: 0, duration: 2, stagger: 0.2, ease: "power4.out" });
+
+}
+
+
+const animateTextChars = (item) => {
+  let textChars = item.querySelectorAll("span");
+
+  gsap.fromTo(
+    arrow,
+    {
+      x: -20,
+      autoAlpha: 0,
+    },
+    {
+      duration: 1,
+      delay: 0.6,
+      x: 0,
+      ease: "power4.out",
+      autoAlpha: 1,
+    }
+  );
+
+
+  gsap.fromTo(
+    textChars,
+    {
+      x: initialPosition,
+      transformOrigin: "top center",
+      rotationX: 75,
+      rotationY: -20,
+      autoAlpha: 0,
+    },
+    {
+      duration: 1,
+      x: 0,
+      stagger: 0.05,
+      ease: "power4.out",
+      rotationX: 0,
+      rotationY: 0,
+      autoAlpha: 1,
+    }
+  );
+};
 
 const init = () => {
-  const sliderItems = document.querySelectorAll('.slider__item');
+  const sliderItems = document.querySelectorAll('.slider .slider__item');
   const intervalDuration = 6000; // 6 segundos
-  const curtains = document.querySelectorAll('.curtain');
+  const itemsTexts = document.querySelectorAll('.slider__item .main-title');
+  const itemLastText = document.querySelector('.slide__text.country');
   let timeout = null;
 
   let activeIndex = 0;
   let tl = gsap.timeline({ paused: true });
 
-  const drawLines = () => {
-    const lines = document.querySelectorAll('.line');
 
-    gsap.set(lines, { backgroundColor: '#D6FFAC' });
+  [...itemsTexts].forEach((item) => {
+    setSplitText(item);
+    animateTextChars(item);
+  });
 
-    gsap.fromTo(lines, { yPercent: 100 }, { yPercent: 0, duration: 2, stagger: 0.2, ease: "power4.out" });
-
-  }
-  
-  drawLines();
-
-
-  // Función que reproduce la animación al cargar la página
-const playAnimationOnLoad = () => {
-  const activeCurtains = document.querySelectorAll('.slider__item.active .curtain');
-  gsap.to(activeCurtains, { left: "-25vw", stagger: 0.2, duration: 0.5, ease: "power1.inOut", onComplete: () => tl.play() });
-};
-
-// Reproduce la animación al cargar la página
-// playAnimationOnLoad();
 
   // Función que cambia el elemento activo
   const changeActive = () => {
@@ -58,15 +99,15 @@ const playAnimationOnLoad = () => {
   
     // Agrega los tweens para abrir y cerrar las cortinas
     tl.clear()
-      .set(previousCurtains, { left: '0' })
-      .to(previousCurtains, { left: '-25vw', duration: 0.5, stagger: 0.2, ease: "power1.inOut" })
-      .set(previousCurtains, { left: '-25vw' })
-      .set(activeCurtains, { left: '0' })
-      .to(activeCurtains, { left: "25vw", duration: 0.5, stagger: 0.2, ease: "power1.inOut" })
-      .to(activeCurtains, { left: "0", duration: 0.5, delay: 2.5, stagger: {
-        each: 0.2,
+      .set(previousCurtains, { left: '0', top: '0'})
+      .to(previousCurtains, { left: !isMobile ? '-25vw' : null, top: isMobile ? '-25vh' : null ,  duration: 0.4, stagger: 0.1, ease: "Power1.easeOut" })
+      .set(previousCurtains, { left: !isMobile ? '-25vw' : null, top: isMobile ? '-25vh' : null  })
+      .set(activeCurtains, { left: '0', top: '0' })
+      .to(activeCurtains, { left: !isMobile ? '25vw' : null, top: isMobile ? '25vh' : null,  duration: 0.4, stagger: 0.1, ease: "Power1.easeOut" })
+      .to(activeCurtains, { left: "0", top: '0', duration: 0.4, delay: 3.9, stagger: {
+        each: 0.1,
         from: "end"
-      }, ease: "power1.inOut" })
+      }, ease: "Power1.easeOut" })
       .set(previousCurtains, { left: '-25vw' });
   
     // Inicia la animación del timeline
@@ -92,15 +133,18 @@ const playAnimationOnLoad = () => {
 
   // Añade la clase "active" al primer elemento
   sliderItems[activeIndex].classList.add('active');
-  // Obtiene las cortinas del primer slide y las abre
-  const activeCurtains = sliderItems[activeIndex].querySelectorAll('.curtain');
-  // gsap.set(activeCurtains, { left: '-25vw' });
-  // gsap.to(activeCurtains, { left: "-25vw", stagger: 0.2, duration: 0.5, ease: "power1.inOut"});
 
-  // setInterval(changeActive, intervalDuration) // 6 segundos
 };
 
 // Inicia la aplicación
-init();
+
+drawLines();
+
+
+setTimeout(() => {
+  init();
+}, 400);
+
+
 
     
