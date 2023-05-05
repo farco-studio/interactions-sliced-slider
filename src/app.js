eb9eb037ccc0e5346e9a4c6a8864ae8b1fd3e0b5
Marina Gallardo <marinagallardo@gmail.com>
2023-05-05 09:57:41 +0200

    cleaning code and optimizing image sizes

--- src/app.js
+++ src/app.js
@@ -1,25 +1,30 @@
-import { cursorComponent } from "./cursor";
 import { gsap } from "gsap";
 import { setSplitText } from "./setSplitText";
 
 const isMobile = window.matchMedia("(max-width: 768px)").matches;
-const arrow = document.querySelector('.slider__content svg');
+const arrow = document.querySelector(".slider__content svg");
 const initialPosition = -260;
+const sliderItems = document.querySelectorAll(".slider .slider__item");
+const slideInterval = 6000; // 6 segundos
+const itemsTexts = document.querySelectorAll(".slider__item .main-title");
+const lines = document.querySelectorAll(".line");
+const animationDuration = 1;
+const animationDelay = 0.6;
+
+let timeout = null;
+let activeIndex = 0;
+let tl = gsap.timeline({ paused: true });
 
 const drawLines = () => {
-  const lines = document.querySelectorAll('.line');
+  gsap.set(lines, { backgroundColor: "#D6FFAC", yPercent: 100 });
+  gsap.fromTo(
+    lines,
+    { yPercent: 100 },
+    { yPercent: 0, duration: 2, stagger: 0.2, ease: "power4.out" }
+  );
+};
 
-  console.log('lines', lines);
-
-  gsap.set(lines, { backgroundColor: '#D6FFAC', yPercent: 100 });
-  gsap.fromTo(lines, { yPercent: 100 }, { yPercent: 0, duration: 2, stagger: 0.2, ease: "power4.out" });
-
-}
-
-
-const animateTextChars = (item) => {
-  let textChars = item.querySelectorAll("span");
-
+const animateArrow = (arrow) => {
   gsap.fromTo(
     arrow,
     {
@@ -27,14 +32,17 @@ const animateTextChars = (item) => {
       autoAlpha: 0,
     },
     {
-      duration: 1,
-      delay: 0.6,
+      duration: animationDuration,
+      delay: animationDelay,
       x: 0,
       ease: "power4.out",
       autoAlpha: 1,
     }
   );
+};
 
+const animateTextChars = (item) => {
+  let textChars = item.querySelectorAll("span");
 
   gsap.fromTo(
     textChars,
@@ -46,7 +54,7 @@ const animateTextChars = (item) => {
       autoAlpha: 0,
     },
     {
-      duration: 1,
+      duration: animationDuration,
       x: 0,
       stagger: 0.05,
       ease: "power4.out",
@@ -57,97 +65,111 @@ const animateTextChars = (item) => {
   );
 };
 
-const init = () => {
-  const sliderItems = document.querySelectorAll('.slider .slider__item');
-  const intervalDuration = 6000; // 6 segundos
-  const itemsTexts = document.querySelectorAll('.slider__item .main-title');
-  const itemLastText = document.querySelector('.slide__text.country');
-  let timeout = null;
+const timelineAnimation = (activeCurtains, previousCurtains) => {
+  const duration = 0.4;
+  const stagger = 0.1;
+  const ease = "Power1.easeOut";
+  const mobileOffset = isMobile ? "25vh" : null;
+  const desktopOffset = !isMobile ? "25vw" : null;
 
-  let activeIndex = 0;
-  let tl = gsap.timeline({ paused: true });
+  tl.restart();
+  tl.clear()
+    .set(previousCurtains, { left: "0", top: "0" })
+    .to(previousCurtains, {
+      left: desktopOffset,
+      top: mobileOffset,
+      duration,
+      stagger,
+      ease,
+    })
+    .set(previousCurtains, { left: desktopOffset, top: mobileOffset })
+    .set(activeCurtains, { left: "0", top: "0" })
+    .to(activeCurtains, {
+      left: desktopOffset,
+      top: mobileOffset,
+      duration,
+      stagger,
+      ease,
+    })
+    .to(activeCurtains, {
+      left: "0",
+      top: "0",
+      duration,
+      delay: 3.9,
+      stagger: {
+        each: stagger,
+        from: "end",
+      },
+      ease,
+    })
+    .set(previousCurtains, { left: "-25vw" });
 
-
-  [...itemsTexts].forEach((item) => {
-    setSplitText(item);
-    animateTextChars(item);
-  });
-
-
-  // Función que cambia el elemento activo
-  const changeActive = () => {
-    resetInterval();
-    // Remueve la clase "active" del elemento actual
-    const previousIndex = activeIndex;
-    sliderItems[previousIndex].classList.remove('active');
-  
-    // Incrementa el índice del elemento activo
-    activeIndex++;
-    // Si el índice supera el número de elementos, vuelve al primero
-    if (activeIndex >= sliderItems.length) {
-      activeIndex = 0;
-    }
-    // Añade la clase "active" al nuevo elemento activo
-    sliderItems[activeIndex].classList.add('active');
-  
-    // Obtiene todos los elementos .curtain dentro del slide activo
-    const activeCurtains = sliderItems[activeIndex].querySelectorAll('.curtain');
-    const previousCurtains = sliderItems[previousIndex].querySelectorAll('.curtain');
-  
-    // Detiene la animación actual y reinicia el timeline
-    tl.pause().restart();
-  
-    // Agrega los tweens para abrir y cerrar las cortinas
-    tl.clear()
-      .set(previousCurtains, { left: '0', top: '0'})
-      .to(previousCurtains, { left: !isMobile ? '-25vw' : null, top: isMobile ? '-25vh' : null ,  duration: 0.4, stagger: 0.1, ease: "Power1.easeOut" })
-      .set(previousCurtains, { left: !isMobile ? '-25vw' : null, top: isMobile ? '-25vh' : null  })
-      .set(activeCurtains, { left: '0', top: '0' })
-      .to(activeCurtains, { left: !isMobile ? '25vw' : null, top: isMobile ? '25vh' : null,  duration: 0.4, stagger: 0.1, ease: "Power1.easeOut" })
-      .to(activeCurtains, { left: "0", top: '0', duration: 0.4, delay: 3.9, stagger: {
-        each: 0.1,
-        from: "end"
-      }, ease: "Power1.easeOut" })
-      .set(previousCurtains, { left: '-25vw' });
-  
-    // Inicia la animación del timeline
-    tl.play();
-  };
-
-  // Función que reinicia el intervalo
-  const resetInterval = () => {
-    // Si existe un intervalo, lo elimina
-    if (timeout) {
-      clearTimeout(timeout);
-    }
-    // Inicia un nuevo intervalo
-  };
-
-  // Función que cambia el elemento activo
-  timeout = setTimeout(() => {
-    changeActive();
-    setInterval(changeActive, intervalDuration) // 6 segundos
-  }, 400);
-
-  
-
-  // Añade la clase "active" al primer elemento
-  sliderItems[activeIndex].classList.add('active');
-
+  tl.play();
 };
 
-// Inicia la aplicación
+const getNextIndex = (currentIndex, arrayLength) => {
+  return (currentIndex + 1) % arrayLength;
+};
+
+const toggleActiveClass = (element) => {
+  element.classList.toggle("active");
+};
+
+const updateActiveSlide = () => {
+  let previousIndex = activeIndex;
+
+  resetTimeout();
+  
+  toggleActiveClass(sliderItems[previousIndex]);
+
+  activeIndex++;
+
+  activeIndex = getNextIndex(activeIndex, sliderItems.length);
+
+  toggleActiveClass(sliderItems[activeIndex]);
+
+  let activeCurtains = sliderItems[activeIndex].querySelectorAll(".curtain");
+  let previousCurtains =
+    sliderItems[previousIndex].querySelectorAll(".curtain");
+
+  timelineAnimation(activeCurtains, previousCurtains);
+};
+
+const resetTimeout = () => {
+  if (timeout) {
+    clearTimeout(timeout);
+  }
+};
+
+const changeSlide = () => {
+  timeout = setTimeout(() => {
+    updateActiveSlide();
+    setInterval(updateActiveSlide, slideInterval); // 6 segundos
+  }, 400);
+};
+
+const addActiveClass = () => {
+  sliderItems[activeIndex].classList.add("active");
+};
+
+const charsAnimation = () => {
+  [...itemsTexts].forEach((item) => {
+    setSplitText(item);
+    animateArrow(arrow);
+    animateTextChars(item);
+  });
+};
+
+const sliderAnimation = () => {
+  charsAnimation();
+  changeSlide();
+  addActiveClass();
+};
 
 setTimeout(() => {
   drawLines();
 }, 200);
 
-
-
 setTimeout(() => {
-  init();
+  sliderAnimation();
 }, 600);
-
-
-
-    
